b87d68fc97da458250e23ae48e30c1dac6e042ee
randiaz95 <randiaz95@gmail.com>
2019-04-05 14:49:15 -0400

    Update ex31.py

--- ex31.py
+++ ex31.py
@@ -22,3 +22,18 @@ if door == "1":
 		print("Bear runs away.")
 elif door == "2":
 	print("You stare into the endless abyss at Cthulhu's retina.")
+	print("1. Blueberries.")
+	print("2. Yellow jacket clothespins.")
+	print("3. Understanding revolvers yelling melodies.")
+
+	insanity = input("> ")
+
+	if insanity == "1" or insanity == "2":
+		print("Your body survives powered by a mind of jello.")
+		print("Good job!")
+	else:
+		print("The insanity rots your eyes into a pool of muck.")
+		print("Good job!")
+
+else:
+	print("You stumble around and fall on a knife and die. Good job!")
