72f7d74861cc006bbec92a1d82d8a73576c5802d
buckl <bucklerco@gmail.com>
2019-04-18 09:52:52 -0400

    base for react gui

--- super_api/super_api.py
+++ super_api/super_api.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from SQLAlchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 
 app = Flask(__name__)
-engine = create_engine("sqlite:///memory.db")
+engine = SQLAlchemy.create_engine("sqlite:///memory.db")
 
 @app.route('/', methods=['GET'])
 def index():
@@ -18,15 +18,17 @@ def index():
 	"""
 	return render_template("index.html", tables=pd.read_sql(engine, all_tables=all_tables_query))
 
-@app.route('/create', methods=['POST'])
+@app.route('/create_table', methods=['POST'])
 def create_table():
-	return f"CREATE TABLE {request.form["table_name"]} ({','.join(request.form["columns"].split())});"
+	return f"CREATE TABLE {request.form['table_name']} ({','.join(request.form['columns'].split())});"
 
-@app.route('/<table>', methods=['GET'])
-def read_all(table):
+@app.route('read_table/<table>', methods=['GET'])
+def read_table(table):
 	return pd.read_sql(engine, "SELECT * FROM data;")
 
-@app.route('/<id>', methods=['GET'])
+## CHECK IF I can just put nothing for ID
+## If so, then just delete read_one and just use read_table
+@app.route('read_one/table/<id>', methods=['GET'])
 def read_one(id):
 	return pd.read_sql(engine, f"SELECT * FROM data WHERE did={id}")
 
@@ -38,12 +40,12 @@ def insert_one():
 def insert_one():
 	return f"INSERT INTO data VALUES ({','.join(request.form.values())});"
 
-@app.route('/u/<id>', methods=['POST'])
+@app.route('/update_table/<id>', methods=['PUT'])
 def update_one(id):
 	return ""
 
-@app.route('/d/<id>', methods=['POST'])
-def delete_one(id):
+@app.route('/delete_table/<id>', methods=['POST'])
+def delete_table(id):
 	return ""
 
 
