2ad3459393a224e79c617badd7540e15283b50e9
buckl <bucklerco@gmail.com>
2019-04-21 17:31:30 -0400

    Finished ex51

---
+++ socket_example/socket/socket/websocket_server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
+
+@socketio.on('my event')
+def handle_my_custom_event(arg1, arg2, arg3):
+    print('received args: ' + arg1 + arg2 + arg3)
+    
+if __name__ == '__main__':
+    socketio.run(app)
